c655a6fe9fc678253e1c2e56f1e4f58956bca395
Valentyna K <uzarevich@ukr.net>
2024-02-07 14:45:56 -0500

    styling adjustments

--- app.py
+++ app.py
@@ -4,19 +4,22 @@ from flask import Flask, jsonify, render_template, request
 from sklearn.metrics.pairwise import cosine_similarity
 import pickle
 
-# popular_df=pickle.load(open('Books-Recommendation-System\popular.pkl', 'rb'))
+# popular_df=pickle.load(open('popular.pkl', 'rb'))
 rating_input_df=pickle.load(open('rating_input.pkl', 'rb'))
 books_df=pickle.load(open('books_df.pkl', 'rb'))
+# search_df=pickle.load(open('search.pkl', 'rb'))
 ratings_df_all_cols=pickle.load(open('ratings_df_all_cols.pkl', 'rb'))
+ratings_df_algo_input=pickle.load(open('ratings_df_algo_input.pkl', 'rb'))
+books_df_algo_input=pickle.load(open('books_df_algo_input.pkl', 'rb'))
 
 #import model
 svd_default_model_mean = pickle.load(open('svd_default_model_mean.pickle', 'rb'))
-
 
 #################################################
 # Flask Setup
 #################################################
 app=Flask(__name__)
+
 # #################################################
 # # Flask Routes
 # #################################################
@@ -37,6 +40,7 @@ def recommend_by_book_ui():
 
 @app.route("/recommend_book", methods=['post'])
 def recommend_book():
+    
     user_book=request.form.get("user_book")
     df_books_ratigs_user=rating_input_df.pivot_table(index='Book-Title', columns='User-ID', values='Book-Rating')
     # filling n/a with 0 so far, assuming it means that no interest for a book by a user,
@@ -80,9 +84,9 @@ def recommend_user():
     model=svd_default_model_mean
     # find those titles that we consider for predictions (e.g. not read by a user)
     # find the books (titles) that were rated and presumably read by a user
-    rated_titles=[i for i in rating_input_df.loc[rating_input_df['User-ID']==user_id,'Book-Title']]
+    rated_titles=[i for i in ratings_df_algo_input.loc[ratings_df_algo_input['User-ID']==user_id,'Book-Title']]
     # find all the titles
-    all_titles=rating_input_df['Book-Title'].unique()
+    all_titles=ratings_df_algo_input['Book-Title'].unique()
     # separate those titles that were not read
     titles_input_to_recommend=[i for i in all_titles if i not in rated_titles]
 
@@ -98,7 +102,7 @@ def recommend_user():
     top_recommendations=predictions_book.head(recommendations_count)
         
     # populate books with full info, selecting those books with the most recent year of publication
-    recommendations_full_info=pd.merge(top_recommendations, books_df, left_on='Book-Title',right_on='Book-Title', how='left')
+    recommendations_full_info=pd.merge(top_recommendations, books_df_algo_input, left_on='Book-Title',right_on='Book-Title', how='left')
     dict_years=dict(recommendations_full_info.groupby('Book-Title')['Year-Of-Publication'].max())
     for i, row in recommendations_full_info.iterrows():
         if row['Year-Of-Publication']!=dict_years[row['Book-Title']]:
@@ -107,6 +111,7 @@ def recommend_user():
     recommendations_full_info=recommendations_full_info.drop_duplicates(subset=['Book-Title'])
     data = recommendations_full_info.to_dict('records')
      # find original user ratings
+    print(ratings_df_all_cols.columns)
     u_data=ratings_df_all_cols[ratings_df_all_cols['User-ID']==user_id].sort_values(by='Book-Rating', ascending=False)
     u_data=u_data[['Book-Title','Book-Rating','Book-Author','Publisher']]
     if u_data.shape[0]>10:
@@ -114,17 +119,16 @@ def recommend_user():
     user_history = u_data.to_dict('records')
     return render_template("recommend_by_user.html", data=data, datau=user_history, user_id=user_id)
 
-@app.route('/search', methods=['GET'])
-def view_books():
-    search_query = request.args.get('search', '')      
-    if search_query:
-        
-        filtered_books_df = books_df[books_df['Book-Title'].str.contains(search_query, case=False, na=False)]
-        books = filtered_books_df.to_dict('records')
-    else:
-        books = books_df.to_dict('records')
+# @app.route('/search', methods=['GET'])
+# def view_books():
+#     search_query = request.args.get('search', '')      
+#     if search_query:
+#         filtered_books_df = search_df[search_df['Book-Title'].str.contains(search_query, case=False, na=False)]
+#         books = filtered_books_df.to_dict('records')
+#     else:
+#         books = search_df.to_dict('records')
 
-    return render_template('search.html', books=books)
+#     return render_template('search.html', books=books)
 
 
 
